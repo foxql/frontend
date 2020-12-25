0e817fe2b323ae072f36180918e48864fcb4b849
boraozer <nagayapim@gmail.com>
2020-12-25 19:49:31 +0300

    installed simple foxql client injection app prop

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,8 @@
 <script>
-	export let name;
+	export let client;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
 	<p>hekır necmettin iş başında</p>
 </main>
 
@@ -13,13 +12,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import App from './App.svelte';
 
+import foxql from 'foxql';
+
+const client = new foxql();
+
+client.pushEvents([
+    'onDocument',
+    'onSearch'
+])
+
+client.use('storageOptions', {
+    saveInterval : true
+});
+
+client.open();
+
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
+		client: client
 	}
 });
 
