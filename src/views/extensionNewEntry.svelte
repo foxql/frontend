7d8ecb20c753d5514e1b2512bac4e29d0a660f0a
boraozer <nagayapim@gmail.com>
2021-02-05 15:25:20 +0300

    added extension page

---
+++ src/views/extensionNewEntry.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let client;
+
+    document.querySelector('.left-side').remove();
+    document.querySelector('.right-side').remove();
+    document.querySelector('.search-box').remove();
+
+    import NewEntry from '../components/newEntry.svelte';
+</script>
+
+<style>
+    body {
+        background : transparent !important;
+    }
+</style>
+
+<NewEntry client = {client}/>
