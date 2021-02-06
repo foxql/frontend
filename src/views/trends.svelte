0f37cc2a0da25d8847ebbab9da36f43e035eb7f6
boraozer <nagayapim@gmail.com>
2021-02-06 10:04:15 +0300

    change trends view

--- src/views/trends.svelte
+++ src/views/trends.svelte
@@ -1,7 +1,14 @@
 <script>
     export let client;
     import Trends from '../components/trends.svelte';
+
+    import MobileCheck from '../helpers/mobileDeviceCheck';
+    import Console from '../components/console.svelte';
 </script>
+
+{#if MobileCheck()}
+    <Console client = {client}/>
+{/if}
 
 <div class = "card rounded-8 card-bg-primary m-t-1">
     <Trends client = {client}/>
