8257f53d7b7b321456a45c462361c06804a5dc8f
boraozer <nagayapim@gmail.com>
2021-02-07 21:04:45 +0300

    all views added page-header

--- src/views/profile.svelte
+++ src/views/profile.svelte
@@ -2,6 +2,8 @@
     export let client;
     
     import CardButtons from '../components/cardButtons.svelte';
+    import Header from '../components/header.svelte';
+    import enums from '../enums/enums.js';
 
     let entrys = Object.values(client.database.useCollection('entrys').documents);
 </script>
@@ -12,7 +14,12 @@
     }
 </style>
 
-{#each entrys as entry}
+<Header content = "{enums.PROFILE}"/>
+
+
+
+<div class = "pd-l-1 pd-r-1">
+    {#each entrys as entry}
 
     <div class = "card pd-1 doc-{entry.documentId} card-bg-primary m-t-1 rounded-8">
         <div class = "card-title pd-b-05">
@@ -25,7 +32,8 @@
         </div>
     </div>
 
-{/each}
+    {/each}
+</div>
 
 
 
--- src/views/share.svelte
+++ src/views/share.svelte
@@ -1,20 +1,25 @@
 <script>
     import NewEntry from '../components/newEntry.svelte';
     import enums from '../enums/enums.js';
+    import Header from '../components/header.svelte';
 
     export let client;
 </script>
 
-<div class = "card pd-1 card-bg-primary m-t-05 rounded-8">
-    <div class = "card-title pd-b-1">
-        {enums.VIEWS.HOME.TITLE}
+<Header content = "{enums.SHARE}"/>
+
+<div class = "pd-l-1 pd-r-1">
+    <div class = "card pd-1 card-bg-primary m-t-05 rounded-8">
+        <div class = "card-title pd-b-1">
+            {enums.VIEWS.HOME.TITLE}
+        </div>
+
+        <div class = "card-body">
+            <p>
+                {@html enums.VIEWS.HOME.BODY}
+            </p>
+        </div>
     </div>
 
-    <div class = "card-body">
-        <p>
-            {@html enums.VIEWS.HOME.BODY}
-        </p>
-    </div>
+    <NewEntry client = {client} />
 </div>
-
-<NewEntry client = {client} />
