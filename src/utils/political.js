d68e29b606458f594c9ac5816e811a9020075fe7
boraozer <nagayapim@gmail.com>
2021-02-07 21:04:30 +0300

    created blacklist modules

---
+++ src/utils/political.js
@@ -0,0 +1,11 @@
+export default {
+    "akp" : "POLITICIAL_PARTY",
+    "chp" : "POLITICIAL_PARTY",
+    "hdp" : "POLITICIAL_PARTY",
+    "mhp" : "POLITICIAL_PARTY",
+    "rte" : "POLITICIAN",
+    "akepe" : "POLITICIAL_PARTY",
+    "cehape" : "POLITICIAL_PARTY",
+    "hedepe" : "POLITICIAL_PARTY",
+    "zhp" : "POLITICIAL_PARTY"
+}
