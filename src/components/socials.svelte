8fed49e5db92e9363171b10df764e8b385a283fe
boraozer <nagayapim@gmail.com>
2021-02-21 05:39:46 +0300

    social media buttons position

--- src/components/socials.svelte
+++ src/components/socials.svelte
@@ -3,13 +3,13 @@
         font-size:13px;
         text-decoration: none;
     }
+    .github-btn, .twitter-btn {
+        width : 50%;
+        text-align: center;
+    }
     .github-btn {
         background : #24292e;
         color : #f6f8fa;
-    }
-    .discord-btn {
-        background : #7289DA;
-        color : #FFFFFF;
     }
 
     .twitter-btn {
@@ -48,10 +48,9 @@
 </style>
 
 <div class = "card pd-1 card-bg-primary m-t-1 rounded-8 ">
-    <div class = "card-body flex f-all-center">
-        <a href = "https://github.com/foxql/foxql" class = "github-btn pd-l-1 pd-r-1 pd-t-05 pd-b-05 rounded-4 m-r-1">Github</a>
-        <a href = "https://discord.gg/8djcRrV2fV" class = "discord-btn pd-l-1 pd-r-1 pd-t-05 pd-b-05 rounded-4 m-r-1">Discord</a>
-        <a href = "https://twitter.com/foxqlcom" class = "twitter-btn pd-l-1 pd-r-1 pd-t-05 pd-b-05 rounded-4 m-r-1">Twitter</a>
+    <div class = "card-body flex f-all-center pd-l-1 pd-r-1">
+        <a href = "https://github.com/foxql/foxql" class = "github-btn pd-l-1 pd-r-1 pd-t-05 pd-b-05 rounded-4">Github</a>
+        <a href = "https://twitter.com/foxqlcom" class = "twitter-btn pd-l-1 pd-r-1 pd-t-05 pd-b-05 rounded-4 m-l-1">Twitter</a>
     </div>
 </div>
 
