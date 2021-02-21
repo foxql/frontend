9b181db62cb2444f0c6751c0a62cef27e5b5dedd
boraozer <nagayapim@gmail.com>
2021-02-21 13:03:31 +0300

    add producthunt icon

--- src/views/share.svelte
+++ src/views/share.svelte
@@ -17,7 +17,10 @@
 <Header content = "{enums.SHARE}"/>
 
 <div class = "pd-l-1 pd-r-1">
-
+    
+    <div class = "card m-t-1 flex f-all-center">
+        <a href="https://www.producthunt.com/posts/foxql?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-foxql" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=285394&theme=dark" alt="FoxQL - P2P social network, like twitter. | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
+    </div>
     <AlertBox contents = {enums.INFORMATIONS}/>
 
     <div class = "card pd-1 card-bg-primary m-t-05 rounded-8">
