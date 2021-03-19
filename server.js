b2201ba5cb5ae70a7c4eb9036d2ee5b729b1fd0e
boraozer <nagayapim@gmail.com>
2021-03-19 23:42:33 +0300

    added server & env

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+var express = require('express')
+var app = express()
+
+app.use(express.static('public'))
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
