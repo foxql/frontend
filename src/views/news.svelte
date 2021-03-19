4ed82bdba0e7579a7df993657d28f7b199e906ef
boraozer <nagayapim@gmail.com>
2021-03-20 01:12:44 +0300

    auto cacher in news page

--- src/views/news.svelte
+++ src/views/news.svelte
@@ -3,6 +3,7 @@
     import EntryBox from '../components/box/entryBox.svelte';
     import InfoBox from '../components/box/infoBox.svelte';
     import lang from '../utils/lang'
+    import { notifier } from '@beyonk/svelte-notifications'
 
     const listenerName = 'actionList';
 
@@ -10,8 +11,25 @@
 
     let actions = [];
 
+    const collection = client.database.useCollection('entrys');
 
     client.peer.socket.on(listenerName, (list)=>{
+        let dedectedNewDocument = false;
+        list.forEach(item => {
+            if(item.type == 'new-document') {
+                const doc = item.document;
+                if(collection.documents[doc.documentId] === undefined) {
+                    collection.addDoc(doc)
+                    dedectedNewDocument = true;
+                }
+            }
+        });
+
+
+        if(dedectedNewDocument) {
+            notifier.success(lang.APP.CACHED_NEW_ENTRYS, 1200)
+        }
+
         actions = list;
     })
 
