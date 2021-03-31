00f324714912594ed285c3582853ab0e0eeb60d4
boraozer <nagayapim@gmail.com>
2021-04-01 01:40:08 +0300

    otomatik klonlama kaldırıldı

--- src/views/entry.svelte
+++ src/views/entry.svelte
@@ -58,8 +58,6 @@
 
     async function query() {
 
-        let clonedDocuments = false;
-
         const queryObject = {
             ref : id,
             collection : 'entrys',
@@ -98,23 +96,6 @@
             send.results.sort((a,b)=>{
                 return new Date(a.doc.createDate) - new Date(b.doc.createDate);
             });
-
-            send.results.forEach((item)=>{
-                const doc = item.doc;
-                const documentId = doc.documentId;
-
-                if(collection.documents[documentId] == undefined) {
-                    clonedDocuments = true;
-
-                    collection.addDoc(doc)
-                }
-                
-            }); 
-
-
-            if(clonedDocuments) {
-                notifier.success(lang.APP.CACHED_NEW_ENTRYS)
-            }
 
         }
 
