9bb21b3d7d77ec4acf4daddcbb55036fe8b395ba
boraozer <nagayapim@gmail.com>
2021-04-30 22:58:02 +0300

    refactoring is done news view

--- src/views/news.svelte
+++ src/views/news.svelte
@@ -1,6 +1,10 @@
 <script>
     export let client;
-    import EntryBox from '../components/box/entryBox.svelte';
+
+    import Entry from '../components/entry/entry.svelte'
+    import EntryHeader from '../components/entry/header.svelte'
+    import Post from '../components/entry/post.svelte'
+
     import InfoBox from '../components/box/infoBox.svelte';
     import lang from '../utils/lang'
 
@@ -11,15 +15,34 @@
         return new Date(b.createDate) - new Date(a.createDate);
     }).slice(0,10)
 
+    actions = actions.map(item => {
+        item.comments = [];
+        return item;
+    })
+
 </script>
 
 
 {#if actions.length > 0}
 
     {#each actions as item}
-        <EntryBox  data = {{
-            doc : item
-        }} client = {client} />
+        <Entry>
+            <div slot = "header">
+                <EntryHeader 
+                    title = {item.title}
+                    navigate = {{
+                        documentId : item.documentId,
+                        entryKey : item.entryKey
+                    }}
+                />
+            </div>
+            <div slot = "posts">
+                <Post 
+                    {...item}
+                    collection = {collection}
+                />
+            </div>
+        </Entry>
 
     {/each}
 
