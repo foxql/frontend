1ec841c36637b40eaf630f1f8fe9b812703d92e3
boraozer <nagayapim@gmail.com>
2021-04-30 22:57:45 +0300

    optional navigate params

--- src/components/entry/header.svelte
+++ src/components/entry/header.svelte
@@ -1,10 +1,24 @@
 
-<div class = "title">
-    {title}
-</div>
+{#if typeof navigate == 'object'}
+
+    <div class = "title">
+        <a href = "entry/{navigate.documentId}/{navigate.entryKey}" use:link>
+            {title}
+        </a>
+    </div>
+
+    {:else} 
+    <div class = "title">
+        {title}
+    </div>
+{/if}
 
 <script>
     export let title;
+    export let navigate = false;
+
+    import { link } from "svelte-routing"
+
 </script>
 
 <style>
@@ -12,5 +26,18 @@
         font-size : 1.2rem;
         font-weight : 600;
         padding-bottom: 0.5rem;
+    }
+
+    a {
+        color : #eee;
+    }
+    
+    
+    @media screen and (max-width: 992px) { 
+
+        .title {
+            font-size: 1rem;
+        }
+
     }
 </style>
