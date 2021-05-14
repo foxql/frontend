63295396be4dad255a0324636159f93d757a0a69
boraozer <nagayapim@gmail.com>
2021-05-14 04:13:46 +0300

    add InfoBox component in offers view

--- src/views/offers.svelte
+++ src/views/offers.svelte
@@ -1,1 +1,12 @@
-Offers sayfasındasın :)
+<InfoBox
+    {...lang.INFO_CARD.OFFERS}
+/>
+
+
+
+<script>
+    export let client;
+    import lang from '../utils/lang';
+
+    import InfoBox from '../components/box/infoBox.svelte';
+</script>
