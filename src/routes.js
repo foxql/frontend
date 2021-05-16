27a3183abb7400e9293bab13c9057f7b4a8d78bc
boraozer <nagayapim@gmail.com>
2021-05-16 03:54:23 +0300

    new routes is done

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 import Entry from './views/entry.svelte';
-import Home from './views/home.svelte';
+import Discovery from './views/discovery.svelte';
 import Share from './views/share.svelte';
 import News from './views/news.svelte';
 import Node from './views/node.svelte';
 import Search from './views/search.svelte';
 import Trends from './views/trends.svelte';
+import Home from './views/home.svelte';
 
 export default [
     {
@@ -16,8 +17,8 @@ export default [
         component : Home
     },
     {
-        path : '/home',
-        component : Home
+        path : '/discovery',
+        component : Discovery
     },
     {
         path : '/share',
